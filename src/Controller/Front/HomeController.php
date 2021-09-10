418e03fdf64ab5359704b3181b0fd7ca87c39cdc
Jules Pauly <paulyjules@gmail.com>
2021-09-10 16:01:03 +0200

    add bootstrap scss

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -11,7 +11,7 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('front/home/index.html.twig', [
+        return $this->render('front/page/home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
