a412522da89dfedf9139a6e05fea50edac90c961
Jules Pauly <paulyjules@gmail.com>
2021-09-10 15:36:53 +0200

    separate base for front and back

--- src/Controller/Front/HomeController.php
+++ src/Controller/Front/HomeController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Front;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,7 +11,7 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('front/home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
