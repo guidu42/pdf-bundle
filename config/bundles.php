f5132a1471155e31b963ffbd0ee9b56bc1ea34be
Jules Pauly <paulyjules@gmail.com>
2021-09-13 15:23:39 +0200

    add drosalys object extension

--- config/bundles.php
+++ config/bundles.php
@@ -15,4 +15,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    DrosalysWeb\ObjectExtensions\Bridge\Symfony\DrosalysWebObjectExtensionsBundle::class => ['all' => true],
 ];
