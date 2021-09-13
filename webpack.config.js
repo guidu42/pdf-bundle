02729bdaaa6b155176341a364b74ded2015db3d4
Jules Pauly <paulyjules@gmail.com>
2021-09-13 14:33:01 +0200

    add ckeditor with configuration ok

--- webpack.config.js
+++ webpack.config.js
@@ -73,6 +73,15 @@ Encore
         to: 'images/shared/[path][name].[ext]'
     })
 
+    .copyFiles([
+        {from: './node_modules/ckeditor/', to: 'ckeditor/[path][name].[ext]', pattern: /\.(js|css)$/, includeSubdirectories: false},
+        {from: './node_modules/ckeditor/adapters', to: 'ckeditor/adapters/[path][name].[ext]'},
+        {from: './node_modules/ckeditor/lang', to: 'ckeditor/lang/[path][name].[ext]'},
+        {from: './node_modules/ckeditor/plugins', to: 'ckeditor/plugins/[path][name].[ext]'},
+        {from: './node_modules/ckeditor/skins', to: 'ckeditor/skins/[path][name].[ext]'},
+        {from: './node_modules/ckeditor/vendor', to: 'ckeditor/vendor/[path][name].[ext]'}
+    ])
+
 
     //Backend style and scripts
     .addEntry('back-scripts', './assets/back/scripts/index.ts')
