589e86b7fd3114637df684a243102240130b8341
Jules Pauly <paulyjules@gmail.com>
2021-09-13 13:11:43 +0200

    add user entity with auth + longin + register + logout + condition logs nav-bar

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -4,10 +4,13 @@ namespace App\Entity;
 
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 
+
+#[UniqueEntity(fields: 'email'), UniqueEntity(fields: 'id')]
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
@@ -24,9 +27,8 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column(type: 'string', length: 180)]
     private $lastName;
 
-    #[ORM\Column(type: 'string')]
+    #[ORM\Column(type: 'array')]
     private $roles = [];
-
 
     #[ORM\Column(type: 'string')]
     private $password;
