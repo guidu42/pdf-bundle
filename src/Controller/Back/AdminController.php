2a98f2da8f76aa073701d0ad7618426a76a65c1b
jordybastide <jordy.bastide@gmail.com>
2021-09-15 09:14:44 +0200

    knp-menu-bundle

--- src/Controller/Back/AdminController.php
+++ src/Controller/Back/AdminController.php
@@ -3,14 +3,27 @@
 namespace App\Controller\Back;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AdminController extends AbstractController
 {
+    private TranslatorInterface $translator;
+
+    /**
+     * @param TranslatorInterface $translator
+     */
+    public function __construct(TranslatorInterface $translator)
+    {
+        $this->translator = $translator;
+    }
+
     #[Route('/admin', name: 'admin')]
     public function index(): Response
     {
+        $trad = $this->translator->trans('page.admin.dashboard.title.label');
+
         return $this->render('back/page/admin/index.html.twig', [
 
         ]);
